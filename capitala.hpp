245c4f148e71e7e45a46e6ba493df781accfb2af
adam1299 <74258205+adam1299@users.noreply.github.com>
2021-01-17 18:33:30 +0200

    Add files via upload

---
+++ capitala.hpp
@@ -0,0 +1,17 @@
+#ifndef CAPITALA_HPP
+#define CAPITALA_HPP
+#include "oras.hpp"
+
+class Capitala:public Oras
+{
+        char *tara;
+public:
+	Capitala();
+	Capitala(const char*,int,int,int  ,const char* );
+	~Capitala();
+	Capitala(const  Capitala&);
+	Capitala& operator=(const Capitala&);
+	void afisare();
+};
+#endif
+
